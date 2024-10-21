d283661b8ee7195330a60bbfd47cb33f5e180ddc
Gunnar Siréus <gunnar.sireus@gmail.com>
2024-10-21 09:49:53 +0200

    Refactoring PhotoDetails

--- src/components/photodetails/PhotoDetails.jsx
+++ src/components/photodetails/PhotoDetails.jsx
@@ -17,9 +17,8 @@ const PhotoDetails = () => {
     const fetchPhotos = async (id) => {
       if (id === 0) {
         try {
-          const response = await apiClient.getHelper('api/photodetails/savedphotoid');
-          const randomPhotoId = Number(response);
-          getAllPhotosInAlbumByPhotoId(randomPhotoId);
+          const randomPhotoId = await apiClient.getHelper('api/photodetails/savedphotoid');
+          getAllPhotosInAlbumByPhotoId(Number(randomPhotoId));
         } catch (error) {
           alert('Could not contact server ' + error);
         }
@@ -84,21 +83,21 @@ const PhotoDetails = () => {
     <div className="container container-fluid">
       <Row>
         <Col className="row-height">
-          <Col md={3} className="hidden-md hidden-sm hidden-xs col-md-height col-md-top custom-vertical-left-border custom-vertical-right-border grey-background">
+          <Col md={3} className="col-md-height grey-background custom-border">
             <Row>
               <Col md={12}>
                 <h4>{albumCaption}</h4>
               </Col>
             </Row>
           </Col>
-          <Col md={9} sm={9} xs={9} className="col-md-height">
+          <Col md={9} className="col-md-height">
             <Row>
               <div className="buttonbar buttonbar-top">
-                <Col lg={2} md={2} sm={2} xs={2} />
-                <Col lg={3} md={3} sm={3} xs={3}>
+                <Col lg={2} />
+                <Col lg={3}>
                   <Link to="/albums">
                     <img id="FormView1_Image1" src="/assets/images/button-gallery.gif" style={{ borderWidth: '0px' }} alt="" />
-                  </Link> &nbsp;&nbsp;&nbsp;&nbsp;
+                  </Link>
                   <Link to="#" onClick={(e) => setPhotoDetailsRoute(e, first)}>
                     <img src="/assets/images/button-first.gif" style={{ borderWidth: '0px' }} alt="" />
                   </Link>
@@ -112,18 +111,18 @@ const PhotoDetails = () => {
                     <img src="/assets/images/button-last.gif" style={{ borderWidth: '0px' }} alt="" />
                   </Link>
                 </Col>
-                <Col lg={7} md={7} sm={7} xs={7} />
+                <Col lg={7} />
               </div>
             </Row>
             <Row>
-              <Col lg={12} md={12} sm={12} xs={12}>
-                <table className="view" cellSpacing="0" cellPadding="0" id="FormView1" style={{ borderCollapse: 'collapse', borderStyle: 'None', borderWidth: '0px' }}>
+              <Col lg={12}>
+                <table className="view" cellSpacing="0" cellPadding="0" style={{ borderCollapse: 'collapse', borderWidth: '0px' }}>
                   <tbody>
                     <tr>
                       <td>
-                        <p>{photos[photoNumber > 0 ? photoNumber - 1 : 0].caption}</p>
+                        <p>{photos[photoNumber - 1]?.caption}</p>
                         <PhotoFrame>
-                          <img src={`${apiAddress}/RandomHandler/Index/PhotoID=${photoId}/Size=L`} className="photo_198" style={{ border: '4px solid white', objectFit: 'contain', minHeight: '500px', maxHeight: '500px', top: '50%', bottom: '50%' }} alt={`PhotoID ${photoId}`} />
+                          <img src={`${apiAddress}/RandomHandler/Index/PhotoID=${photoId}/Size=L`} className="photo_198" style={{ border: '4px solid white', objectFit: 'contain', minHeight: '500px', maxHeight: '500px' }} alt={`PhotoID ${photoId}`} />
                         </PhotoFrame>
                         <p>
                           <a href={`${apiAddress}/RandomHandler/Download/${photoId}/Size=L`}>
@@ -139,11 +138,11 @@ const PhotoDetails = () => {
             </Row>
             <Row>
               <div className="buttonbar buttonbar-bottom">
-                <Col md={4} sm={3} xs={2}>{clickList}</Col>
-                <Col md={3} sm={3} xs={3}>
+                <Col md={4}>{clickList}</Col>
+                <Col md={3}>
                   <Link to="/albums">
                     <img id="FormView1_Image2" src="/assets/images/button-gallery.gif" style={{ borderWidth: '0px' }} alt="" />
-                  </Link> &nbsp;&nbsp;&nbsp;&nbsp;
+                  </Link>
                   <Link to="#" onClick={(e) => setPhotoDetailsRoute(e, first)}>
                     <img src="/assets/images/button-first.gif" style={{ borderWidth: '0px' }} alt="" />
                   </Link>
@@ -157,7 +156,7 @@ const PhotoDetails = () => {
                     <img src="/assets/images/button-last.gif" style={{ borderWidth: '0px' }} alt="" />
                   </Link>
                 </Col>
-                <Col md={5} sm={6} xs={7} />
+                <Col md={5} />
               </div>
             </Row>
           </Col>
@@ -166,5 +165,6 @@ const PhotoDetails = () => {
     </div>
   );
 };
+
 
 export default PhotoDetails;
